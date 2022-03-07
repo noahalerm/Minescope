017d1c37f8916bc9a085c6f334c286ac568bc112
fgdfgfgdg <fgdfgdfg>
2022-03-07 15:41:24 +0100

    SampleFragmentBase

---
+++ app/src/main/java/com/example/minescope/SampleFragment.kt
@@ -0,0 +1,11 @@
+package com.example.minescope
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+
+class SampleFragment : Fragment(R.layout.fragment_sample) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?){
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
