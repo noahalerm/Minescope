371f48596273ccd839a2d91209d5f143469c6a62
noah <noah.alerm.7e4@itb.cat>
2022-03-18 18:34:09 +0100

    Filters Fragment Implementation

---
+++ app/src/main/java/com/example/minescope/ui/views/FiltersFragment.kt
@@ -0,0 +1,13 @@
+package com.example.minescope.ui.views
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.minescope.R
+
+class FiltersFragment : Fragment(R.layout.fragment_filters) {
+    //ON VIEW CREATED
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
