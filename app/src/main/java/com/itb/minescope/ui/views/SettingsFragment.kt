ba60b3177c3e0af6d5c9e191469bb235276b354d
noah <noah.alerm.7e4@itb.cat>
2022-05-07 03:21:05 +0200

    Settings Language Functionality Implementation

--- app/src/main/java/com/itb/minescope/ui/views/SettingsFragment.kt
+++ app/src/main/java/com/itb/minescope/ui/views/SettingsFragment.kt
@@ -1,25 +1,23 @@
 package com.itb.minescope.ui.views
 
+import android.content.res.Configuration
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import android.widget.ImageView
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.itb.minescope.R
-import com.itb.minescope.ui.viewmodel.MinescopeViewModel
 
+import java.util.Locale
 
 class SettingsFragment : Fragment(R.layout.fragment_settings) {
     //ATTRIBUTES
     //Layout Elements
     private lateinit var closeBtn: ImageView
     private lateinit var themeInput: AutoCompleteTextView
-
-    //View Model
-    private val viewModel: MinescopeViewModel by activityViewModels()
 
     //ON VIEW CREATED
     override fun onViewCreated(view: View, savedInstanceState: Bundle?){
@@ -35,7 +33,30 @@ class SettingsFragment : Fragment(R.layout.fragment_settings) {
 
         //ON CLICK
         closeBtn.setOnClickListener {
+            Log.d("LANGUAGE", themeInput.text.toString())
+            setLanguage(themeInput.text.toString())
             findNavController().popBackStack()
         }
+    }
+
+    //METHODS
+    /**
+     * This method is used to set the app's language.
+     * @param lang Chosen language
+     */
+    private fun setLanguage(lang: String) {
+        var locale = Locale.getDefault()
+
+        when (lang) {
+            getString(R.string.en_lang) -> locale = Locale("en")
+            getString(R.string.es_lang) -> locale = Locale("es")
+            getString(R.string.cat_lang) -> locale = Locale("ca")
+        }
+
+        Locale.setDefault(locale)
+        val resources = activity!!.resources
+        val config: Configuration = resources.configuration
+        config.setLocale(locale)
+        resources.updateConfiguration(config, resources.displayMetrics)
     }
 }
