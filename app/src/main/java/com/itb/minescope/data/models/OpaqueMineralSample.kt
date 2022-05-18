a61329069644a22c7360045b081ab05df99c62d2
noah <noah.alerm.7e4@itb.cat>
2022-05-19 01:49:50 +0200

    Update

--- app/src/main/java/com/itb/minescope/data/models/OpaqueMineralSample.kt
+++ app/src/main/java/com/itb/minescope/data/models/OpaqueMineralSample.kt
@@ -1,3 +1,3 @@
 package com.itb.minescope.data.models
 
-data class OpaqueMineralSample(var id: Int, var id_mineral: Int, val lpa: String, val lpna: String, var nom: String, var coloracio: String, var pleocroisme: String, var abundancia: Float, var altres_minerals: String, var forma: String, var exfoliacio_polit: String, var reflectivitat: String, var resistencia_polit: String, var anisotropia: String, var colors_interferencia: String, var reflexions_internes: String)
+data class OpaqueMineralSample(var id: Int, var id_mineral: Int, val lpa: String, val lpna: String, var nom: String, var coloracio: String, var pleocroisme: String, var abundancia: String, var altres_minerals: String, var forma: String, var exfoliacio_polit: String, var reflectivitat: String, var resistencia_polit: String, var anisotropia: String, var colors_interferencia: String, var reflexions_internes: String)
